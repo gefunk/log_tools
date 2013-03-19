be9f4aa7671df85b41adda5e149ebf2e99ff19ff
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-03-19 15:52:00 -0400

    Added Login library and validation

--- application/views/admin/contract/add.php
+++ application/views/admin/contract/add.php
@@ -1,6 +1,8 @@
 <div class="row-fluid">
 	<div class="span10">
-		<form>
+		<?php echo validation_errors(); ?>
+
+		<?php echo form_open('admin/contract/add'); ?>
 		  <fieldset>
 		    <legend>New Contract</legend>
 		    
@@ -14,7 +16,7 @@
 		    <span class="help-block">Select a customer for the contract</span>
 		    
 			<label>Contract Number</label>
-			<input type="text" name="contract_number" value="" id="contract_number" placeholder="Contract Number" />
+			<input type="text" name="contract_number" value="<?php echo set_value('contract_number'); ?>" id="contract_number" placeholder="Contract Number" />
 			<span class="help-block">Enter the contract number on the contract</span>
 			
 			<label>Carrier</label>
@@ -27,11 +29,11 @@
 			<span class="help-block">Select the carrier this contract belongs to</span>
 			
 			<label>Start Date</label>
-			<input type="text" class="datepicker" name="start_date" value="" id="start_date"  placeholder="Start Date" />
+			<input type="text" class="datepicker" name="start_date" value="<?php echo set_value('start_date'); ?>" id="start_date"  placeholder="Start Date" />
 			<span class="help-block">Start date on the contract</span>
 			
 			<label>End Date</label>
-			<input type="text" class="datepicker" name="end_date" value="" id="end_date" placeholder="End Date" />
+			<input type="text" class="datepicker" name="end_date" value="<?php echo set_value('end_date'); ?>" id="end_date" placeholder="End Date" />
 			<span class="help-block">End date on the contract</span>
 			
 			
