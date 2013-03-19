77b49371ca1fa598fbf2ef1b197aac759d2c0842
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-03-18 22:11:38 -0400

    extra commits from today

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -1,5 +1,16 @@
-		</div>
+		</div> <!-- end of the container div -->
 		<script src="http://code.jquery.com/jquery.js"></script>
 		<script src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
+		<script src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.js"></script>
+		
+		<?php
+		// way to pass in javascript to append to end of document
+		if(isset($scripts)){
+			foreach($scripts as $script):
+		?>
+			<script src="<?php echo base_url(); ?>assets/js/<?php echo $script; ?>"></script>
+		<?php 
+			endforeach; 
+		}?>
 	</body>
 </html>
--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -1,12 +1,20 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 	<head>
 		<meta http-equiv="Content-type" content="text/html; charset=utf-8">
 		<title>Export Code Search</title>
 		 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<!-- Bootstrap -->
 		<link href="<?php echo base_url(); ?>assets/css/bootstrap.min.css" rel="stylesheet" media="screen" />
-		<link href="<?php echo base_url(); ?>assets/css/export_code_search.css" rel="stylesheet" media="screen" />
 		<link href="<?php echo base_url(); ?>assets/css/bootstrap-responsive.css" rel="stylesheet">
+		<link href="<?php echo base_url(); ?>assets/css/datepicker.css" rel="stylesheet">
 	</head>
 	<body>
-		<div class="container">
+		<div class="container-fluid">
+			<div class="row-fluid">
+				<div class="span2">
+					<ul class="nav nav-pills nav-stacked">
+						<li><a href="#">Add Contract</a></li>
+						<li><a href="#">Remove Contract</a></li>
+					</ul>
+				</div>
