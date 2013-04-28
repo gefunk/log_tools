82518f789436ab924a7228e6a5833aa35761d69a
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-04-27 22:50:24 -0400

    Added changes to landing page
    Landing page

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,6 @@
-		</div> <!-- end of the container div -->
+		
+			</div> <!-- end of the container div -->
+		</div><!-- end content div -->
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/jquery.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap.min.js"></script>
 		<script type="text/javascript" src="<?php echo base_url(); ?>assets/js/bootstrap-datepicker.js"></script>
