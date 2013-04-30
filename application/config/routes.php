377b85d77c5f77700eec4caa634ac3a31d42a8ce
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-04-30 16:40:45 -0400

    missing parentheses

--- application/config/routes.php
+++ application/config/routes.php
@@ -42,8 +42,8 @@ $route['default_controller'] = "welcome";
 $route['404_override'] = '';
 
 $route['contact'] = "welcome/contact";
-$route['register/:num'] = "main/register/$1";
-$route['signin/:num'] = "main/signin/$1";
+$route['register/(:num)'] = "main/register/$1";
+$route['signin/(:num)'] = "main/signin/$1";
 
 
 /* End of file routes.php */
