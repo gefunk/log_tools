99b8b3259cfd724ead8db28388c3ab5ab10f6756
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-05-20 15:39:57 -0400

    Changed to use database procedures instead of sql
    Changed from mysql driver to mysqli driver, safer

--- application/models/querymodel.php
+++ application/models/querymodel.php
@@ -26,19 +26,21 @@ class QueryModel extends CI_Model
 		$origin_closest_ports = "";
 		$origin_data = array();
 		foreach($origin_closest as $port){
-			$origin_closest_ports .= $port->id.",";
-			$origin_data[$port->id] = $port->distance;
+			error_log("Port ID: ".$port["id"]);
+			$origin_closest_ports .= $port['id'].",";
+			$origin_data[$port['id']] = $port['distance'];
 		}
 		$origin_closest_ports = rtrim($origin_closest_ports, ',');
 		
 		$dest_closest_ports = "";
 		$dest_data = array();
 		foreach($destination_closest as $port){
-			$dest_closest_ports .= $port->id.",";
-			$dest_data[$port->id] = $port->distance;
+			$dest_closest_ports .= $port['id'].",";
+			$dest_data[$port['id']] = $port['distance'];
 		}
 		$dest_closest_ports = rtrim($dest_closest_ports, ',');
-		
+		error_log("Origin Closest: ".$origin_closest_ports);
+		error_log("Destination Closest: ".$dest_closest_ports);
 		$lanes = $this->get_lanes_match_origin_destination($origin_closest_ports, $dest_closest_ports, $customer_id);
 		
 		$lane_ids = array();
@@ -95,31 +97,21 @@ class QueryModel extends CI_Model
 			$leg_type = 2;
 		}
 		
-		$sql =  	"SELECT "
-					."port.id,"
-					."3956 * 2 * ASIN(SQRT( POWER(SIN((city.latitude - port.latitude)* pi()/180 / 2), 2) + COS(city.latitude * pi()/180) * 	COS(port.latitude * pi()/180) * POWER(SIN((city.longitude - port.longitude) * pi()/180 / 2), 2) )) as distance "
-					."from "
-						."ref_cities city,"
-						."ref_ports port "
-					."where "
-						."city.id = $city_id "
-					."and port.found = 1 "
-					."and port.id in( "
-						."select distinct(location) "
-						."from contract_lane_legs cll "
-						."where cll.`leg_type` = $leg_type "
-						."and contract_lane in("
-							."select id "
-							."from contract_lanes cl "
-							."where contract = (select con.id from contracts con where con.customer = $customer_id)"
-						.")"
-					.")"
-				  	."order by distance";
+		$sql = "CALL find_closest_ports(?,?,?)";
+		$params = array($city_id, $leg_type, $customer_id);
 				
-		$query = $this->db->query($sql);
-		
-		return $query->result();
-		
+		$result = $this->db->query($sql, $params);
+		if(($result) && ($result->num_rows() > 0))
+		{
+			
+		   $row = $result->result_array();
+		} 
+		else 
+			$row =array('result'=>1,'message'=>'Something went horribly wrong with the DB','last_id'=>0); // Database Problem - die gracefully.
+
+		$result->next_result(); // Dump the extra resultset.
+		$result->free_result(); // Does what it says.
+		return $row; // Return the row to the controller that called it.
 		
 	}
 
