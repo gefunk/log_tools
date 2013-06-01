7eb0648c3c7320e36dc9f61c82bc31052610bcf1
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-05-31 22:01:58 -0400

    Adding application config into repo
    adding config into repo

---
+++ application/config/upload.php
@@ -0,0 +1,7 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+$config['upload_path'] = './upload/';
+
+$config['allowed_types'] = 'gif|jpg|png|jpeg|pdf|xls|doc|docx';
+
+?>
