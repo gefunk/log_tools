e915df07ae823effdd75d63247ff2fd33caf5ba7
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-07-02 10:53:10 -0400

    Fixing cache driver for production

--- application/config/production/memcached.php
+++ application/config/production/memcached.php
@@ -1,19 +1,19 @@
 <?php   if (!defined('BASEPATH')) exit('No direct script access allowed');
 
-/**
+
 $config['memcached'] = array(
  'hostname' => 'bridgetown.bfh6oy.cfg.use1.cache.amazonaws.com',
  'port' => 11211,
  'weight' => 1
 ); 
-**/
+
 
 /**
  * Config file retrieved from here 
  *
  * http://ellislab.com/forums/viewthread/180005/#852413
  */
-
+/**
 $config= array(
     'bridgetwon' => array(
         'host' => 'bridgetown.bfh6oy.cfg.use1.cache.amazonaws.com',
@@ -21,4 +21,4 @@ $config= array(
         'weight'    => 1
     )
 ); 
-
+*/
