29140c0ccf3bdfe9f05554580971b3e54a1a0dfb
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-07-02 10:49:52 -0400

    Updated production memcached config

--- application/config/production/memcached.php
+++ application/config/production/memcached.php
@@ -1,5 +1,24 @@
+<?php   if (!defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
 $config['memcached'] = array(
  'hostname' => 'bridgetown.bfh6oy.cfg.use1.cache.amazonaws.com',
  'port' => 11211,
  'weight' => 1
 ); 
+**/
+
+/**
+ * Config file retrieved from here 
+ *
+ * http://ellislab.com/forums/viewthread/180005/#852413
+ */
+
+$config= array(
+    'bridgetwon' => array(
+        'host' => 'bridgetown.bfh6oy.cfg.use1.cache.amazonaws.com',
+        'port'        => 11211,
+        'weight'    => 1
+    )
+); 
+
