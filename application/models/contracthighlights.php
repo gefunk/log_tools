f7532a9de139b285400c9b6d8503229248161404
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-07-15 23:20:28 -0400

    Added ability to highlight
    highlighting ability

--- application/models/contracthighlights.php
+++ application/models/contracthighlights.php
@@ -8,16 +8,21 @@ class ContractHighlights extends CI_Model
         parent::__construct();
     }
 	
-	public function add($contract_id, $position, $page)
+	public function add($contract_id, $position, $height, $page)
 	{
-		$data = array("contract" => $contract_id, "position" => $position, "page" => $page);
+		$data = array(
+			"contract" => $contract_id,
+			"position" => $position,
+			"height" => $height, 
+			"page" => $page
+		);
 		$this->db->insert("contract_highlights", $data);
 		return $this->db->insert_id();
 	}
 	
 	public function get_for_contract_page($contract_id, $page)
 	{
-		$this->db->select("id, position");
+		$this->db->select("id, position, height");
 		$this->db->where("contract", $contract_id);
 		$this->db->where("page", $page);
 		$this->db->from("contract_highlights");
