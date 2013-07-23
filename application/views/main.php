ec0ce553e6438eebbeab4f5efdf7e4c378e1a869
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-07-23 11:21:20 -0400

    Working drop down
    working drop down morning commit

--- application/views/main.php
+++ application/views/main.php
@@ -1,5 +1,16 @@
 <div class="row-fluid">
-  <input id="origin" placeholder="Start City" />
-  
-  
+	<div class="span10">
+	  
+	</div>
+	<div class="span5">
+	  <input id="origin" placeholder="Origin"/>
+	</div>
+	<div class="span5">
+	  <input id="destination" placeholder="Destination" />
+	</div>
+		
+	
+  	<div class="span10">
+		<button id="press" class="btn-flat btn-primary">Search</button>
+	</div>
 </div>
