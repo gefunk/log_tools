50e330f627ec2ea26445b2c9127d2ff26273e5bb
Rahul Gokulnath <rahul.gokulnath@turner.com>
2013-07-25 14:37:49 -0400

    Checking cookie fix in production
    cookies were not working because customer id was not being saved,
    testing this in production

--- application/views/admin/signin.php
+++ application/views/admin/signin.php
@@ -1,14 +1,26 @@
+<div class="row-fluid">
 
+	<div id="messages">
+		<?php echo $this -> session -> flashdata("messages"); ?>
+	</div>
 
-<form action="<?php echo site_url('admin/login/verify_admin_user'); ?>" method="post" accept-charset="utf-8">
- <fieldset>
-    <legend>Administrator Login...</legend>
-    <label>type in your email</label>
-    <input type="text" placeholder="Email Address">
-    <span class="help-block">Example block-level help text here.</span>
-    <label>type in your password</label>
-    <input type="password" placeholder="Password">
-    <span class="help-block">The password you log in with</span>
-    <button type="submit" class="btn">Submit</button>
-  </fieldset>	
-</form>
+	<form action="<?php echo site_url('admin/login/verify_admin_user'); ?>" method="post" accept-charset="utf-8">
+		<fieldset>
+			<legend>
+				Administrator Login...
+			</legend>
+			<label>type in your email</label>
+			<input name="email" type="text" placeholder="Email Address">
+			<span class="help-block">Type in your email</span>
+			<label>type in your password</label>
+			<input name="password" type="password" placeholder="Password">
+			<span class="help-block">The password you log in with</span>
+			<input type="hidden" name="remember" value="true" />
+			<button type="submit" class="btn">
+				Submit
+			</button>
+		</fieldset>
+
+	</form>
+
+</div>
