38478db834ad233369a7202b33eea6fe670f4a9c
Rahul Gokulnath <rahul.gokulnath@turner.com>
2014-01-27 16:19:08 -0500

    Cleaned up legacy stuff
    There was a bunch of stuff for testing and functions that are not being
    used, removed them from the codebase

--- application/controllers/main.php
+++ application/controllers/main.php
@@ -12,16 +12,6 @@ class Main extends MY_In_Controller {
 		$this->load->model('lanemodel');
     }
 
-	public function oldindex()
-	{
-		$header_data['title'] = "Query Rates";
-		$header_data['page_css'] = array('query.css', 'lib/select2.css');
-		$footer_data['scripts'] = array('select2.js', 'query.js', 'custom-selectors/cities.selector.js', 'custom-selectors/ports.selector.js');
-		$this->load->view('header', $header_data);
-		$this->load->view('queryrates');
-		$this->load->view('footer', $footer_data);
-	}
-	
 	public function index()
 	{
 		$header_data['title'] = "Query Rates";
@@ -45,29 +35,6 @@ class Main extends MY_In_Controller {
 	}
 	
 
-
-	public function sprites(){
-		$this->load->view("testsprite");
-	}
-	
-	
-	public function testsearchlanes()
-	{
-		$this->output->enable_profiler(TRUE);
-		$origin_city_id = 2457684;
-		$destination_city_id = 2338691;
-		$customer_id = 9;
-		$search_lanes = ($this->querymodel->search_lanes($origin_city_id, $destination_city_id, $customer_id));
-		$search_lanes['lane_detail'] = $this->lanemodel->get_lanes_by_lane_id($search_lanes['lanes']);
-		echo var_dump($search_lanes);
-		
-	}
-	
-	public function testclosest($city_id)
-	{
-		$this->output->enable_profiler(TRUE);
-		echo var_dump($this->querymodel->find_closest_ports_to_city($city_id, 9));
-	}
 }
 
 /* End of file welcome.php */
